603901605410715e60a6b6a434746ba9ad5e60c6
tobiaskane69 <tobiashernandez47@gmail.com>
2023-11-28 21:08:12 +0800

    ambatukaaaaaaam

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btns=[
+
     {
         id: 1,
         name: 'Jordan'
@@ -66,8 +67,151 @@ const product = [
         title: 'Air Jordan 1 Retro High OG',
         price: 178,
         category: 'Jordan',
+    },
+
+    {
+        id: 2,
+        image: 'images/airmax1.jpg',
+        title: 'Nike Air Max Pulse Roam',
+        price: 160,
+        category: 'Airmax',
+    },
+    {
+        id: 2,
+        image: 'images/airmax2.jpg',
+        title: 'Nike Air Max Scorpion Flyknit',
+        price: 245,
+        category: 'Airmax',
+    },
+    {
+        id: 2,
+        image: 'images/airmax3.jpg',
+        title: 'Nike Air VaporMax Moc Roam',
+        price: 212,
+        category: 'Airmax',
+    },
+    {
+        id: 2,
+        image: 'images/airmax4.jpg',
+        title: 'Nike Tiempo Legend 10 Elite',
+        price: 221,
+        category: 'Airmax',
+    },
+    {
+        id: 2,
+        image: 'images/airmax5.jpg',
+        title: 'Nike Air Max 97',
+        price: 146,
+        category: 'Airmax',
+    },
+
+    {
+        id: 3,
+        image: 'images/airforce1.jpg',
+        title: 'Nike Air Force 1 07',
+        price: 99,
+        category: 'Airforce',
+    },
+    {
+        id: 3,
+        image: 'images/airforce2.jpg',
+        title: 'Nike Air Force 1 LV8',
+        price: 90,
+        category: 'Airforce',
+    },
+    {
+        id: 3,
+        image: 'images/airforce3.jpg',
+        title: 'Nike Air Force 1 Low Retro',
+        price: 151,
+        category: 'Airforce',
+    },
+    {
+        id: 3,
+        image: 'images/airforce4.jpg',
+        title: 'Nike Air Force 1 07 Mid',
+        price: 112,
+        category: 'Airforce',
+    },
+    {
+        id: 3,
+        image: 'images/airforce5.jpg',
+        title: 'Nike Air Force 1 PLT.AF.ORM',
+        price: 112,
+        category: 'Airforce',
+    },
+
+    {
+        id: 4,
+        image: 'images/running1.jpg',
+        title: 'Nike Invincible 3',
+        price: 178,
+        category: 'Running',
+    },
+    {
+        id: 4,
+        image: 'images/running2.jpg',
+        title: 'Nike Structure 25',
+        price: 133,
+        category: 'Running',
+    },
+    {
+        id: 4,
+        image: 'images/running3.jpg',
+        title: 'Nike Alphafly 2',
+        price: 261,
+        category: 'Running',
+    },
+    {
+        id: 4,
+        image: 'images/running4.jpg',
+        title: 'Nike Pegasus 40',
+        price: 114,
+        category: 'Running',
+    },
+    {
+        id: 4,
+        image: 'images/running5.jpg',
+        title: 'Nike Pegasus 40 "Eliud Kipchoge"',
+        price: 114,
+        category: 'Running',
+    },
+
+    {
+        id: 5,
+        image: 'images/basketball1.jpg',
+        title: 'LeBron XXI Dragon Pearl EP',
+        price: 196,
+        category: 'Basketball',
+    },
+    {
+        id: 5,
+        image: 'images/basketball2.jpg',
+        title: 'Tatum 1 Home Team PF',
+        price: 124,
+        category: 'Basketball',
+    },
+    {
+        id: 5,
+        image: 'images/basketball3.jpg',
+        title: 'Nike G.T. Jump 2 EP',
+        price: 185,
+        category: 'Basketball',
+    },
+    {
+        id: 5,
+        image: 'images/basketball4.jpg',
+        title: 'Air Jordan XXXVIII PF',
+        price: 196,
+        category: 'Basketball',
+    },
+    {
+        id: 5,
+        image: 'images/basketball5.jpg',
+        title: 'Giannis Immortality 3 EP',
+        price: 77,
+        category: 'Basketball',
     }
-    
 ];
 
 const categories = [...new Set(product.map((item)=>
